9c85b77c88c20bf82c1eaf7fcee8f67bf6b95e72
Devante Trotman <devantetrotman@gmail.com>
2022-01-16 23:21:22 -0600

    Update IncreaseBtns.js

--- src/pomodoro/IncreaseBtns.js
+++ src/pomodoro/IncreaseBtns.js
@@ -12,33 +12,7 @@ function IncreaseBtns({isTimerRunning,
     setBreakDuration,
     setInitialFocusDuration,
     setIsTimerRunning }){
-    function stopTimer(){
-        console.log("stopped timer",session);
-        setSession(null);
-        setIsTimerRunning(false);
-      }
-
-      function playPause() {
-        console.log("play pause",session,isTimerRunning)
-        setInitialFocusDuration(focusDuration);
-        setIsTimerRunning((prevState) => {
-          const nextState = !prevState;
-          if (nextState) {
-            setSession((prevStateSession) => {
-              // If the timer is starting and the previous session is null,
-              // start a focusing session.
-              if (prevStateSession === null) {
-                return {
-                  label: "Focusing",
-                  timeRemaining: focusDuration * 60,
-                };
-              }
-              return prevStateSession;
-            });
-          }
-          return nextState;
-        });
-      }
+    
 
     return (
         <div>
@@ -107,54 +81,6 @@ function IncreaseBtns({isTimerRunning,
           </div>
         </div>
       </div>
-      <div className="row">
-      <div className="col">
-        <div
-          className="btn-group btn-group-lg mb-2"
-          role="group"
-          aria-label="Timer controls"
-        >
-          <button
-            type="button"
-            className="btn btn-primary"
-            data-testid="play-pause"
-            title="Start or pause timer"
-            onClick={playPause}
-          >
-            <span
-              className={classNames({
-                oi: true,
-                "oi-media-play": !isTimerRunning,
-                "oi-media-pause": isTimerRunning,
-              })}
-            />
-          </button>
-          {/* TODO: Implement stopping the current focus or break session. and disable the stop button when there is no active session */}
-          {session === null ? (
-            /* TODO: Disable the stop button when there is no active session */
-          <button
-            type="button"
-            className="btn btn-secondary"
-            data-testid="stop"
-            title="Stop the session"
-            disabled
-          >
-            <span className="oi oi-media-stop" />
-          </button>
-          ) : (
-          <button
-            type="button"
-            className="btn btn-secondary"
-            data-testid="stop"
-            title="Stop the session"
-            onClick={stopTimer}
-          >
-            <span className="oi oi-media-stop" />
-          </button>
-          )}
-        </div>
-      </div>
-    </div>
     </div>
     );
 }
